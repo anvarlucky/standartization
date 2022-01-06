08e1c6e647017ca3bc77fe6090260bee79b940f7
anvarlucky <anvarmath@gmail.com>
2022-01-06 11:02:51 +0500

    bugun

--- resources/views/admin/standarts/show.blade.php
+++ resources/views/admin/standarts/show.blade.php
@@ -1,22 +1,17 @@
 @extends('layouts.mainAdmin')
 @section('content')
-    <div class="container-fluid">
-        <div class="col-md-9 table-box">
-            <b>Qo`llash sohasi</b>
-            <br/>
-            {!! $standart->scope !!}
-            <br/>
-            @if($standart->photo_scope)
-            <img src="/storage/scope/{{$standart->photo_scope}}" width="500" height="400">
-            @endif
-            <br/>
-            <b>Meyoriy havolalar</b>
-            <br/>
-            {!! $standart->normative_references !!}
-            <br/>
-            <a href="{{route('doc',$standart->id)}}">DOC</a>
-            <br/>
-            <a href="{{route('pdf',$standart->id)}}">PDF</a>
-        </div>
-    </div>
+    {!!  $standart->scope!!}
+    <b>Qo`llash sohasi</b>
+    <br/>
+    {!!  $standart->scope!!}
+    <br/>
+    <img src="/storage/scope/{{$standart->photo_scope}}" width="500" height="400">
+    <br/>
+    <b>Meyoriy havolalar</b>
+    <br/>
+    {!!  $standart->normative_references!!}
+    <br/>
+    <a href="{{route('doc',$standart->id)}}">DOC</a>
+    <br/>
+    <a href="{{route('pdf',$standart->id)}}">PDF</a>
 @endsection
